01fa9f7e03b8922709a158693847a2cc07126024
AnuBis7Star <58041069+AnuBis7Star@users.noreply.github.com>
2026-06-03 15:13:07 +0200

    Set base path for Astro site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://anubis7star.github.io/cistelleria-carbonell-astro/',
+  base: '/cistelleria-carbonell-astro',
 });
